c7c3693e5eee9945ccb6c170b1bb88849b416816
ShellySaturne <shellysaturne@gmail.com>
2016-02-27 12:58:38 -0500

    my 99 bottles of beer code

---
+++ 99bottlesofbeer.rb
@@ -0,0 +1,19 @@
+puts "Which song lyric would you like to be displayed?"
+song = gets.chomp #can append downcase after .chomp
+song = song.downcase # can write this as song.downcase!..... it replaces previous song title
+x = "99bob_lyrics.rb"
+#x = File.open("/Users/GaryJean-Joseph/subl/99bob_lyrics.rb", "r")
+#song= x.read
+def lyric (song)
+	
+		if song == '99 bottles of beer on the wall' && File.exist?("99bob_lyrics.rb")
+			x = File.open("/Users/GaryJean-Joseph/subl/99bob_lyrics.rb", "r")
+			song = x.read
+			puts "#{song}"
+		else
+			puts "There are no lyrics stored for this song. Please try again."
+		end
+end
+
+
+lyric(song)
