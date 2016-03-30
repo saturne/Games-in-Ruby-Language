6e29e286eff126fe2c4901ce51b69eb06272ca60
Shelly Saturne <shellysaturne@gmail.com>
2016-03-30 18:27:15 -0400

    Update deafgrandma_bye3x.rb

--- deafgrandma_bye3x.rb
+++ deafgrandma_bye3x.rb
@@ -21,4 +21,10 @@ end
 
 end	 
 	   	
+	   	
+=begin
+Write a program that prints out the complete lyrics to “99 bottles of beer on the wall.”
+Write a Deaf Grandma program. Whatever you say to grandma(user input) she should respond with HUH?!, SPEAK UP SONNY!, unless you shout it(type in all CAPS). If you shout, she can hear you and yells back NO, NOT SINCE 1938! Grandma should shout a different year each time; random between 1930 to 1980. You can’t stop talking to grandma until you shout BYE.
+Add on to the above. Grandma really loves your company and doesn’t want you to go unless you shout BYE three times in a row. So if you say BYE twice and then something else you have to say BYE three times again.
+=end
  
